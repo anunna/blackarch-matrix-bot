c5b5a1b5b253ed1848064614b939be1f4dc46c33
Alia Morozova <anunna@lilitusec.net>
2023-01-17 22:01:21 +0200

    Create room_monitor.py

---
+++ room_monitor.py
@@ -0,0 +1,41 @@
+import asyncio
+from matrix_client.client import MatrixClient
+
+# Create a new client
+client = MatrixClient("https://matrix.org")
+
+# Log in to the server
+async def login():
+    await client.login("access_token", "my_bot")
+
+# Join a room
+async def join_room(room_id):
+    room = await client.join_room(room_id)
+    print(f"Joined room: {room_id}")
+
+# Listen for messages
+async def listen_for_messages():
+    while True:
+        try:
+            event = await client.next_event()
+            if event["type"] == "m.room.member":
+                if event["content"]["membership"] == "join":
+                    user_id = event["sender"]
+                    domain = user_id.split(":")[1]
+                    if domain != "matrix.org":
+                        room = client.get_rooms()[0]
+                        members = [m.split(":")[1] for m in room.get_joined_members() if m != user_id]
+                        if members.count(domain) > 5:
+                            await client.room_ban(room.room_id, domain)
+                            print(f"Banned domain: {domain}")
+        except Exception as e:
+            print(e)
+
+# Run the bot
+async def run():
+    await login()
+    await join_room("!room_id:matrix.org")
+    await listen_for_messages()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run())
